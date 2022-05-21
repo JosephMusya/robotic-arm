e5cb6656934ab8638cd87bffe20b7af11fb7bbbb
JosephMusya <josephmusya254@gmail.com>
2022-05-21 18:37:50 +0300

    Homing, testing, added comments too

--- gui.m
+++ gui.m
@@ -1,3 +1,5 @@
+%auto build function do not change
+%code build from the GUI
 function varargout = gui(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
@@ -15,6 +17,8 @@ if nargout
 else
     gui_mainfcn(gui_State, varargin{:});
 end
+
+%%The following functions makes the specified variables global,
 
 function makeGlobal(my_robot)
 global x
@@ -105,18 +109,24 @@ function r = getMatrix
 global matrixH
 r = matrixH;
 
+function basic_view
+%Set the axis limit
+xlim([-0.4000 0.2000]) 
+ylim([-0.5000 0.1000])  
+zlim([-0.3 0.3])
+%set the view azimuth angles
+view([110 40])
+
+%default view for the robot vie
 function setDefault
 path = 'C:/Users/josep/Desktop/JOSEE 5.1/Project/coding/urdf/urdf/assemblyurdf.SLDASM/urdf/assemblyurdf.SLDASM.urdf';
-my_robot = importrobot(path);
-config = homeConfiguration(my_robot);
+my_robot = importrobot(path); %importing the urdf file as my_robot
+config = homeConfiguration(my_robot);%default view for the robot
 makeGlobal(my_robot)
 makeGlobalc(config)
-show(my_robot,config);
+show(my_robot,config);%Show the robot
+basic_view
 
-xlim([-0.4000 0.1000])
-ylim([-0.4000 0.1000])
-zlim([-0.3 0.3])
-view([110 40])
 function gui_OpeningFcn(hObject, eventdata, handles, varargin)
 setDefault
 
@@ -152,16 +162,13 @@ th4 = get_th4;
 th5 = get_th5;
 
 config(1).JointPosition = deg2rad(th1); %Assign theta 1 to first joint
-config(2).JointPosition = deg2rad(th2);
-config(3).JointPosition = deg2rad(th3);
-config(4).JointPosition = deg2rad(th4);
-config(5).JointPosition = deg2rad(th5);
+config(2).JointPosition = deg2rad(th2); %Assign theta 2 to first joint
+config(3).JointPosition = deg2rad(th3); %Assign theta 3 to first joint
+config(4).JointPosition = deg2rad(th4); %Assign theta 4 to first joint
+config(5).JointPosition = deg2rad(th5); %Assign theta 5 to first joint
 
 show(r,config);
-xlim([-0.4000 0.1000])
-ylim([-0.4000 0.1000])
-zlim([-0.3 0.3])
-view([110 40])
+basic_view
 
 function slider1_Callback(hObject, eventdata, handles)
 th = get(hObject,'Value');
@@ -193,12 +200,13 @@ if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColo
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
-
+%what happens when slider 3 value changes
 function slider3_Callback(hObject, eventdata, handles)
 th = get(hObject,'Value');
 make_th3(th)
 display_robot
 
+%function to create slider 3
 function slider3_CreateFcn(hObject, eventdata, handles)
 
 % Hint: slider controls usually have a light gray background.
@@ -206,57 +214,69 @@ if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColo
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
+%what happens when slider 4 value changes
 function slider4_Callback(hObject, eventdata, handles)
 th = get(hObject,'Value');
 make_th4(th)
 display_robot
 
+%creating slider 4
 function slider4_CreateFcn(hObject, eventdata, handles)
 
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
-
+%what happens when slider 5 value changes
 function slider5_Callback(hObject, eventdata, handles)
 th = get(hObject,'Value');
 make_th5(th)
 display_robot
 
+%creating slider 5
 function slider5_CreateFcn(hObject, eventdata, handles)
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
+%what happens when input is provided in btn1
 function edit1_Callback(hObject, eventdata, handles)
 x_target = str2double(get(hObject,'String'));
 makeX(x_target)
 
+%creating btn1
 function edit1_CreateFcn(hObject, eventdata, handles)
 
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
+%what happens when input is provided in btn2
 function edit2_Callback(hObject, eventdata, handles)
 y_target = str2double(get(hObject,'String'));
 makeY(y_target)
 
+%creating btn 2
 function edit2_CreateFcn(hObject, eventdata, handles)
 
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
+%what happens when input is provided in btn
 function edit3_Callback(hObject, eventdata, handles)
 z_target = str2double(get(hObject,'String'));
 makeZ(z_target)
 
+%creating btn 3
 function edit3_CreateFcn(hObject, eventdata, handles)
 
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+%returns homogenous transformation matrix given the variables(DH
+%parameters)
 function HTM = DHP(th,alph,d,r)
 th = deg2rad(th);
 alph = deg2rad(alph);
@@ -265,10 +285,11 @@ MATX = [
     sin(th) cos(th)*cos(alph) -cos(th)*sin(alph) r*sin(th)
     0 sin(alph) cos(alph) d
     0 0 0 1
-];
+]; %the homogenous transformation matrix
 HTM.MATX = MATX;
 
-
+%returns the joint angels given the end effector position (x,y,z)
+%ori is the orientation of the end effector with respect to the base frame
 function angles = ikine(x,y,z,ori)
 a1 = 1.1903;
 a2 = 0.7120;
@@ -314,43 +335,40 @@ angles.theta3 = theta3;
 angles.theta4 = theta4;
 angles.theta5 = theta5;
 
-
+%function to move the robot
+%mat is a matrix carring the values for the joint angle variables
+%e.g = mat = [angle1 angle2 angle3 angle4 angle5]
 function move(mat,count)
-framesPerSecond = 24;
+framesPerSecond = 24; %frames per second for the animation
 
 r = rateControl(framesPerSecond);
 my_robot = getGlobal;
 config = getGlobalc;
 
-
-for i=1:count
+%starting the animation using a for loop
+for i=1:count 
 th1 = mat(i,1);
 th2 = mat(i,2);
 th3 = mat(i,3);
 th4 = mat(i,4);
 th5 = mat(i,5);
 
+%configure the robot joint with the obtain theta values
 config(1).JointPosition = deg2rad(th1);
 config(2).JointPosition = deg2rad(th2);
 config(3).JointPosition = deg2rad(th3);
 config(4).JointPosition = deg2rad(th4);
 config(5).JointPosition = deg2rad(th5);
 
-
 show(my_robot,config,'PreservePlot',true);
 
-xlim([-0.4000 0.1000])
-ylim([-0.4000 0.1000])
-zlim([-0.3 0.3])
-view([110 40]);
+basic_view
 
 waitfor(r);
 end
-function console
-    fig = uifigure;
-    uialert(fig,'Cant Reach the Point\nOut of workspace','ERROR');
-    
-    
+
+%generates the homing matrix, flips the matrix used to take the robot to
+%the target position
 function new_mat = home_mat(mat)
 angle1 = flip(mat(:,1))'';
 angle2 = flip(mat(:,2))'';
@@ -360,16 +378,21 @@ angle5 = flip(mat(:,5))'';
 
 new_mat = [angle1 angle2 angle3 angle4 angle5];
 
+%what happens when radio button is pressed
 function radiobutton1_Callback(hObject, eventdata, handles)
 x = getX;
 y = getY;
 z = getZ;
+
+%orientationo end effector
 orientation = [
     1 0 0 0
     0 0 -1 0
     0 1 0 0
     0 0 0 1
     ];
+
+%obtain the joint angles
 angles = ikine(x,y,z,orientation);
 
 th1 = (angles.theta1);
@@ -386,22 +409,11 @@ angle3 = linspace(start,th3,count);
 angle4 = linspace(start,th4,count);
 angle5 = linspace(start,th5,count);
 
-if isnan(angle1)
-    console
-elseif isnan(angle2)
-    console
-elseif isnan(angle3)
-    console
-elseif isnan(angle4)
-    console
-elseif isnan(angle5)
-    console
-else
 mat = [angle1;angle2;angle3;angle4;angle5]';
 homing_matrix = home_mat(mat);
 makeMatrixGlobal(homing_matrix);
 move(mat,count);
-end
+
 
 
 % --- If Enable == 'on', executes on mouse press in 5 pixel border.
@@ -415,6 +427,7 @@ function disp_message(message)
 myicon = imread("avatar.png");
 msgbox(message,"Success","custom",myicon); 
 
+%what happens when button is pressed
 function pushbutton1_Callback(hObject, eventdata, handles)
 matrix = getMatrix;
 if isempty(matrix)
@@ -431,6 +444,7 @@ end
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+%test joint angles
 function test_joint(joint,n)
 framesPerSecond = 100;
 
@@ -450,16 +464,13 @@ config(n).JointPosition = deg2rad(th1);
 
 show(my_robot,config,'PreservePlot',true);
 
-xlim([-0.4000 0.1000])
-ylim([-0.4000 0.1000])
-zlim([-0.3 0.3])
-view([110 40]);
+basic_view
 
 waitfor(r);
 end
 config(5).JointPosition = deg2rad(0);
 show(my_robot,config,'PreservePlot',true);
-view([110 40]);
+basic_view
 message = 'Joint Test Successful!';
 disp_message(message)
 
